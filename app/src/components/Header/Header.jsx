8433cc38956d04c0a6da158b1b9897208c2ec942
Aysse <aysse4@gmail.com>
2024-03-11 17:31:06 +0000

    Add header component - uncompleted

---
+++ app/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+import marvelLogo from '../../assets/marvel-logo.svg'
+import heartIcon from '../../assets/heart-icon.svg'
+
+function Header() {
+
+    return (
+        <div className='header-container'>
+            <img src={marvelLogo} className='marvel-logo' alt='Marvel logo' />
+            <div className='fav-div'>
+                <img src={heartIcon} alt="Heart fav icon" />
+                <div className='fav-text'>3</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
