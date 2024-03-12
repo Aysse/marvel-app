aee5f6a7ef28e408c389b924eccb0bc751106de1
Aysse <aysse4@gmail.com>
2024-03-12 13:59:19 +0000

    Delete unnecesary imports

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,11 +3,6 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import GeneralView from './components/GeneralView/GeneralView'
-import Search from './components/Search/Search'
-import { useStateValue } from './context/apiContext'
-import useApiGetInitialData from './useApiGetInitialData'
-import Body from './components/Body/Body'
-import constants from './constants'
 
 function App() {
   return (
