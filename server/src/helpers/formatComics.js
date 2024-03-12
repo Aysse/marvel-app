692fe272325924760a5c02be37b5f8530d9879f1
Aysse <aysse4@gmail.com>
2024-03-12 17:54:54 +0000

    Add comic on sale year in the API response

--- server/src/helpers/formatComics.js
+++ server/src/helpers/formatComics.js
@@ -2,10 +2,14 @@ export function formatComics (data) {
   if (!data || !data.data || !data.data.results) return [];
 
   return data.data.results.map((comic) => {
+    const onSaleDateObj = comic.dates.find((date) => date.type === 'onsaleDate');
+    const onSaleDate = onSaleDateObj ? new Date(onSaleDateObj.date) : '';
+    const onSaleYear = onSaleDate ? onSaleDate.getFullYear() : '';
     return {
       id: comic.id,
       title: comic.title,
       description: comic.description,
+      year: onSaleYear,
       image: {
         path: comic.thumbnail.path,
         extension: comic.thumbnail.extension
