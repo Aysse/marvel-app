f9c884c6f4c7c95f5af4deb49bf6c34501d377ad
Aysse <aysse4@gmail.com>
2024-03-14 13:16:18 +0000

    Change port by default

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,4 +1,4 @@
 export default {
-    GET_CHARACTERS_ENDPOINT: 'http://localhost:3001/characters',
-    GET_CHARACTER_BY_ID_ENDPOINT: 'http://localhost:3001/characters/id'
+    GET_CHARACTERS_ENDPOINT: 'http://localhost:3000/characters',
+    GET_CHARACTER_BY_ID_ENDPOINT: 'http://localhost:3000/characters/id'
 }
